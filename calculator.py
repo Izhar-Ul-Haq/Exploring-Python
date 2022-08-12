711fea98bad607b1e44735e82e1abb3676bd4c5e
Izhar Ul Haq <iizharulhaq9@gmail.com>
2022-08-12 15:27:02 +0500

    simple calculator to learn pytest

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+c = add(4,5)
+d = sub(4,5)
+e = mul(4,5)
+f = div(4,5)
+print(c)
+print(d)
+print(e)
+print(f)
