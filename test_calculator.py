1ce456bf17580755d18e51fa25cc786030a52b66
Izhar Ul Haq <iizharulhaq9@gmail.com>
2022-08-17 22:02:51 +0500

    Testing Calculator

--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,10 @@
 import calculator
 def test_calc_multipy():
 
-    multiply = calculator.mul(4,5)
+    multiply = calculator.mul(4, 5)
     assert multiply==20
 def test_calc_add():
-    addition = calculator.add(40,4)
+    addition = calculator.add(40, 4)
     assert addition == 44
 test_calc_add()
 test_calc_multipy()
